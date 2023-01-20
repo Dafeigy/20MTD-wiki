99e7d2f1612a23df74620244f74e7facd35ca2c4
Dafeigy <1070642765@qq.com>
2023-01-20 15:46:14 +0800

    <Fix>:Reconstruct project structure.

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+from pyecharts import options as opts
+from pyecharts.globals import ThemeType
+from pyecharts.charts import Bar
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("Homepage.html")
+
+@app.route("/Images")
+def Images():
+    return render_template("Images.html")
+
+@app.route("/Tables")
+def Tables():
+    return render_template("Tables.html")
+
+    
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000,debug=True)
+    # ecahrts + flask
---
+++ process_text.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import re
+
+
+
+with open ('static\ORIGIN\TextAsset\localization', 'r', encoding='utf-8') as f:
+    texts =f.readlines()
+
+
+def format_data(line:str):
+    return re.findall('"(.*?)"',line)[:-1]
+    
+df = pd.DataFrame(columns=("key","en","jp","ch","tc","ru","sp","latam","gr","pl","it","tr","fr","br","pr","kr","hu"))
+
+for index, data in enumerate(texts):
+    if index == 0:
+        continue
+    df.loc[index] = format_data(data)
+
+df.to_csv('static\ORIGIN\TextAsset\localization.csv',index=False,encoding='utf_8_sig')
+
